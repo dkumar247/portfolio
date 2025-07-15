72f069ff948b18cc0ed49589ffaa17a5c439b5f6
dkumar247 <dkumar247@gmail.com>
2025-07-16 01:43:19 +0530

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './sections/Hero.jsx'
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
+
+const App = () => {
+    return (
+        <>
+            <NavBar/>
+            <Hero/>
+            <ShowcaseSection/>
+        </>
+    )
+}
+export default App
